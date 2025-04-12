e1817ceda4b92f0c35a2d7b34d0ee97a3b5d3ccf
Bianca de Paula <biancadepaula@MacBook-Air-de-Bianca.local>
2025-04-11 23:04:13 -0300

    add git ignore

--- dist/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.defaultConfig = void 0;
-exports.defaultConfig = {
-    daysToLookBack: 7,
-    language: "pt"
-};
